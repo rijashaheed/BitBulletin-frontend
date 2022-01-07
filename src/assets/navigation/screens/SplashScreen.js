488d1f4bdf1699f74487b27a628b9a9093da4b3b
rijashaheed <rijashaheed@gmail.com>
2022-01-07 12:17:01 +0500

    Initial commit

---
+++ src/assets/navigation/screens/SplashScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>Splash Screen for BitBulletin</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
