f0dbd1879ae455c36646181dd9fd4d1c4591e9ed
AdeenaLathiya <adeenalathiya@gmail.com>
2022-01-12 20:36:49 +0500

    screens navigation

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,14 +1,8 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
 import SplashScreen from '../screens/SplashScreen/SplashScreen';
-// import {SplashScreen} from '../screens/SplashScreen';
 
 import {createStackNavigator} from '@react-navigation/stack';
-
-// const Stack = createStackNavigator();
 
 const Stack = createStackNavigator();
 
@@ -19,18 +13,11 @@ const screenOptions = {
 export default function RootNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Navigator
+        screenOptions={screenOptions}
+        initialRouteName="SplashScreen">
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-
-    // <SplashScreen />
-    // <NavigationContainer>
-    //   <Stack.Navigator
-    //     screenOptions={screenOptions}
-    //     initialRouteName="SplashScreen">
-    //     <Stack.Screen name="SplashScreen" component={SplashScreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
   );
 }
