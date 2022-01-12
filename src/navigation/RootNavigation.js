aa88caeb20ac8d291ff4ead012e74b3d38216e9a
AdeenaLathiya <adeenalathiya@gmail.com>
2022-01-12 17:09:39 +0500

    screens navigation

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,13 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
 import SplashScreen from '../screens/SplashScreen/SplashScreen';
+// import {SplashScreen} from '../screens/SplashScreen';
 
-function RootNavigation() {
+import {createStackNavigator} from '@react-navigation/stack';
+
+// const Stack = createStackNavigator();
+
+const Stack = createStackNavigator();
+
+const screenOptions = {
+  headerShown: false,
+};
+
+export default function RootNavigation() {
   return (
-    <View>
-      <SplashScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    // <SplashScreen />
+    // <NavigationContainer>
+    //   <Stack.Navigator
+    //     screenOptions={screenOptions}
+    //     initialRouteName="SplashScreen">
+    //     <Stack.Screen name="SplashScreen" component={SplashScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
-
-export default RootNavigation;
--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+// import {images} from '../../../resources';
+// import styles from './style';
 
-function SplashScreen() {
-  return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  );
-}
+const SplashScreen = () => (
+  <View>
+    <Text>Hello</Text>
+  </View>
+  // <View style={styles.container}>
+  //   <Image style={styles.image} resizeMode="contain" source={images.logo} />
+  // </View>
+);
 
 export default SplashScreen;
