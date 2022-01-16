b67b017b919c8097d5a7e8796ce5ae272f96a37c
rijashaheed <rijashaheed@gmail.com>
2022-01-16 14:03:44 +0500

    Built NewsList screen

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import SplashScreen from '../screens/SplashScreen/SplashScreen';
+import NewsList from '../screens/NewsList/NewsList';
 
 function RootNavigation() {
   return (
     <View>
-      <SplashScreen />
+      {/* <SplashScreen /> */}
+      <NewsList />
     </View>
   );
 }
