6c2a35afe339835024d00ea71003ff1723b4c14e
AdeenaLathiya <adeenalathiya@gmail.com>
2022-02-28 19:08:35 +0500

    NewsList

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -5,6 +5,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import SplashScreen from '../screens/SplashScreen/SplashScreen';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
+import NewsList from '../screens/NewsList/NewsList';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,7 @@ export default function RootNavigation() {
         initialRouteName="SplashScreen">
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="NewsList" component={NewsList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
