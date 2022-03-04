c37d572d63d8afe65759110504e6b4923dd19a34
AdeenaLathiya <adeenalathiya@gmail.com>
2022-03-04 17:57:03 +0500

    navigation from home page to news list page

--- src/resources/strings.js
+++ src/resources/strings.js
@@ -4,28 +4,33 @@ module.exports.strings = {
   newsList: {
     list: [
       {
+        id: 0,
         title:
           'Omicron cases: Smart lockdown imposed in Karachi’s Gulshan-e-Iqbal area',
         time: '15 min',
         image: images.newsList.image1,
       },
       {
+        id: 1,
         title:
           'South Africa eye victory in third Test despite Pant century for India',
         time: '35 min',
         image: images.newsList.image2,
       },
       {
+        id: 2,
         title: 'Uproar in NA after mini-budget',
         time: '55 min',
         image: images.newsList.image3,
       },
       {
+        id: 3,
         title: 'Pakistan annual inflation rose to 12.3% in December',
         time: '8 hours',
         image: images.newsList.image4,
       },
       {
+        id: 4,
         title: 'Panel formed by Sindh High Court wants ban on cryptocurrency',
         time: '12 hours',
         image: images.newsList.image5,
