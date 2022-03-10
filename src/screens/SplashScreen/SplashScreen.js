0f803708531894e11fdd28d06b2cf6971ac9420c
AdeenaLathiya <adeenalathiya@gmail.com>
2022-03-10 22:54:32 +0500

    ui fixes

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react';
-import {Image, View} from 'react-native';
-import {images} from '../../resources/images';
+import React, { Component } from 'react';
+import { Image, ImageBackground, View } from 'react-native';
+import { images } from '../../resources/images';
 import styles from './Style';
 
 export default class SplashScreen extends Component {
   render() {
-    const {navigation} = this.props;
+    const { navigation } = this.props;
 
     setTimeout(() => {
       navigation.replace('HomeScreen');
     }, 2000);
 
     return (
-      <View style={styles.container}>
-        <Image style={styles.image} resizeMode="contain" source={images.logo} />
-      </View>
+      <ImageBackground
+        style={styles.image}
+        source={images.logo}
+        resizeMode="contain"
+      />
     );
   }
 }
--- src/screens/SplashScreen/Style.js
+++ src/screens/SplashScreen/Style.js
@@ -1,23 +1,9 @@
-import {StyleSheet} from 'react-native';
-import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import { StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-
-  text: {
-    textAlign: 'center',
-  },
-
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
   image: {
-    flex: 1,
     width: wp('100%'),
+    height: hp('100%'),
   },
 });
